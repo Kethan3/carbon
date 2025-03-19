abae4ceff10602c98271add5cd4fcdc6a8a49ea3
Kethan3 <kethangowda003@gmail.com>
2025-03-19 14:17:17 +0530

    add Prisma client integration and update root route response

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client';
 
-const app = new Hono()
+const app = new Hono();
+const prismaClient = new PrismaClient();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('welcome to college database');
+})
+
+app.get('/student',(c)=>{
+const student = prismaClient.student.findMany();
+return c.json({student},200)
+
 })
 
 
 
+serve(app);
 
-serve(app)
+console.log("server is running at http://localhost:3000")
